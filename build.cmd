aefdc7806264c741b7b411ab40cecfdd20014d73
max <bfbf>
2026-05-04 19:05:59 +0000

    x

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 cd /d "%~dp0"
 
-python tools\generate_licenses.py cdfuse\Cargo.toml  cdfuse\THIRD_PARTY_LICENSES.md
+cargo fetch --manifest-path cdwinfs\Cargo.toml
+cargo fetch --manifest-path cdcore\Cargo.toml
+
 python tools\generate_licenses.py cdwinfs\Cargo.toml cdwinfs\THIRD_PARTY_LICENSES.md
 python tools\generate_licenses.py cdcore\Cargo.toml  cdcore\THIRD_PARTY_LICENSES.md
 
--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-python tools/generate_licenses.py cdfuse/Cargo.toml  cdfuse/THIRD_PARTY_LICENSES.md
-python tools/generate_licenses.py cdwinfs/Cargo.toml cdwinfs/THIRD_PARTY_LICENSES.md
-python tools/generate_licenses.py cdcore/Cargo.toml  cdcore/THIRD_PARTY_LICENSES.md
+cargo fetch --manifest-path cdfuse/Cargo.toml
+cargo fetch --manifest-path cdcore/Cargo.toml
+
+python tools/generate_licenses.py cdfuse/Cargo.toml cdfuse/THIRD_PARTY_LICENSES.md
+python tools/generate_licenses.py cdcore/Cargo.toml cdcore/THIRD_PARTY_LICENSES.md
 
 cargo build --release --manifest-path cdcore/Cargo.toml
 cargo build --release --manifest-path cdfuse/Cargo.toml
